77da3c23aa45c2b60d32ed7304856fc472beee75
Alan Avellaneda Silva <64187029+alansito1177@users.noreply.github.com>
2021-06-07 12:37:05 -0400

    git innit

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+const path = require("path");
+require("dotenv").config();
+
+//Local
+console.log(process.env.DATABASE_URL)
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
