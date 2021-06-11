1e9af8dd77eb8a0fe1af711b14aceb11b29e7061
Alan Avellaneda <alansito1177@gmail.com>
2021-06-11 14:26:05 -0400

    simplified code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
-const app = express();
 const logger = require("./config/logger");
 
 const moviesRouter = require('./movies/movies.router');
@@ -12,6 +11,8 @@ const criticsRouter = require('./critics/critics.router');
 
 const notFound = require('./errors/notFound');
 const errorHandler = require('./errors/errorHandler');
+
+const app = express();
 
 app.use(logger);
 app.use(cors());
